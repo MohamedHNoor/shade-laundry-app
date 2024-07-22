9e7b57f200c7865d5212d5af968a9bf2ac6336e2
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-07-22 16:34:14 +0200

    Material top tab navigator

--- app/(admin)/orders/_layout.js
+++ app/(admin)/orders/_layout.js
@@ -3,7 +3,7 @@ import { Stack } from 'expo-router';
 export default function OrderScreen() {
   return (
     <Stack>
-      <Stack.Screen name='index' options={{ title: 'Orders' }} />
+      <Stack.Screen name='list' options={{ headerShown: false }} />
     </Stack>
   );
 }
