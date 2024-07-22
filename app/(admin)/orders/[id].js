475f1af7304d41f1f7ce95827e749a42cca23e18
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-07-22 16:11:37 +0200

    Order status

--- app/(admin)/orders/[id].js
+++ app/(admin)/orders/[id].js
@@ -1,8 +1,10 @@
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import { orders } from '../../../assets/data/orders';
 import OrderItemListItem from '../../../components/OrderItemListItem';
 import OrderListItem from '../../../components/OrderListItem';
+
+const OrderStatusList = ['New', 'Processing', 'Delivered'];
 
 const OrderDetailScreen = () => {
   const { id } = useLocalSearchParams();
@@ -24,6 +26,30 @@ const OrderDetailScreen = () => {
         data={order.order_items}
         renderItem={({ item }) => <OrderItemListItem item={item} />}
         contentContainerStyle={{ gap: 10 }}
+        ListFooterComponent={
+          <>
+            <Text className='font-pbold'>Status</Text>
+            <View className='flex-row space-x-3'>
+              {OrderStatusList.map((status) => (
+                <TouchableOpacity
+                  key={status}
+                  onPress={() => console.warn('Update status')}
+                  className={`border ${
+                    order.status === status ? 'bg-secondary' : 'bg-transparent '
+                  }  border-secondary py-2 px-4 rounded-md my-3`}
+                >
+                  <Text
+                    className={`${
+                      order.status === status ? 'text-white' : 'text-primary'
+                    } font-psemibold`}
+                  >
+                    {status}
+                  </Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+          </>
+        }
       />
     </View>
   );
